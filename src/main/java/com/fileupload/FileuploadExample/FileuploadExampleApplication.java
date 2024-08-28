b7236559d17e6a0f9aaa81e303c82f28a7723ad4
Guzman <francisco.guzman@accenture.com>
2024-08-28 15:54:53 -0400

    lab done

---
+++ src/main/java/com/fileupload/FileuploadExample/FileuploadExampleApplication.java
@@ -0,0 +1,13 @@
+package com.fileupload.FileuploadExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileuploadExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileuploadExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fileupload/FileuploadExample/FileuploadExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fileupload.FileuploadExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileuploadExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
